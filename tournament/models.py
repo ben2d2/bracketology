f35936e59efee23e1fa137cba77bc59a7ee93bd0
Ben Bridges <ben2d2@gmail.com>
2017-11-22 21:02:13 -0600

    Rockin a database and models

--- tournament/models.py
+++ tournament/models.py
@@ -4,3 +4,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Region(models.Model):
+    name = models.CharField(max_length=200)
+
+class Team(models.Model):
+    name = models.CharField(max_length=200)
+    seed = models.IntegerField(default=0)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+class GameTeam(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+class Game(models.Model):
+    game_round = models.IntegerField(default=0)
